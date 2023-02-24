4395efbef7f3d5e86c3fac0b25591fb216ff47de
AlexaDRS <oleksandr.s@int.pl>
2023-02-24 17:29:48 +0100

    0.1

--- PhisicExperiment/Source.cpp
+++ PhisicExperiment/Source.cpp
@@ -2,6 +2,7 @@
 #include <SDL_image.h>
 #include <iostream>
 #include <math.h>
+#include <vector>
 
 int wWidth = 800;
 int wHeight = 600;
@@ -24,45 +25,48 @@ public:
 	double y{ 0 };
 };
 
-class Square
+class Figure
 {
 public:
+	//Points of figure
+	 std::vector <Point> points;
 
-	Point A{ 0,0 };
-	Point B{ 0,0 };
+	//Vector
 	Point V{ 0,0 };
+
 	SDL_Color color;
 
 	double speed = 0;
 
-	Square(double& aX, double& aY, double& bX, double& bY, double& vX, double& vY, SDL_Color& dColor, double dSpeed = 1)
+	Figure(std::vector <Point> sPoints, double vX, double vY, SDL_Color& dColor, double dSpeed = 1)
 	{
-
-		A = { aX, aY };
-		B = { bX, bY };
+		for (int i = 0; i < sPoints.size(); i++)
+		{	
+			points.push_back(sPoints[i]);
+		}
+		std::cout << points.size();
 		V = { vX, vY };
 		color = dColor;
 		speed = dSpeed;
-
-		//swap points for function SDL_RenderFillRect()
-		if (B.y > A.y)
-			std::swap(B.y, A.y);
-		if (B.x < A.x)
-			std::swap(B.x, A.x);
-
 	};
 
 	void Show(int x0, int y0)
 	{
-		double w = abs(B.x - A.x);
-		double h = abs(B.y - A.y);
-		SDL_Rect rect{ x0 + A.x, y0 + B.y, w, h };
 
 		SDL_SetRenderDrawColor(ren, color.r, color.g, color.b, color.a);
 		SDL_RenderDrawRect(ren, &window);
 
-		//	SDL_SetRenderDrawColor(ren, color.r, color.g, color.b, color.a);
-		SDL_RenderFillRect(ren, &rect);
+		Point A{ 0,0 };
+		Point B{ 0,0 };
+
+		for (int i = 0; i < points.size() - 1; ++i)
+		{
+			A = { x0 + points[i].x, y0 - points[i].y };
+			B = { x0 + points[i+1].x, y0 - points[i+1].y };
+			SDL_RenderDrawLine(ren, A.x, A.y, B.x, B.y);
+		}
+		A = { x0 + points[0].x, y0 - points[0].y };
+		SDL_RenderDrawLine(ren, A.x, A.y, B.x, B.y);
 	}
 
 	void Normalize()
@@ -89,54 +93,150 @@ public:
 		//std::cout << "AFTER: " << v.x << " " << v.y << "\n";
 	}
 
+	Point CenterMass()
+	{
+		double mX(0);
+		double mY(0);
+		//double m(0);
+		for (int i = 0; i < points.size(); ++i)
+		{
+			mX += points[i].x;
+			mY += points[i].y;
+		}
+
+		mX /= points.size();
+		mY /= points.size();
+
+		
+		return { mX, mY };
+	}
+
+	void Rotate(double angle, double x0, double y0, Point &C)
+	{
+		double radian = angle * M_PI / 180.0;
+
+		bool isGood = true;;
+		for (int i = 0; i < points.size(); ++i)
+		{
+			Point temp{ 0,0 };
+			Point tempA{ 0,0 };
+
+			tempA.x = points[i].x - C.x;
+			tempA.y = points[i].y - C.y;
+
+			if (tempA.x != 0 || tempA.y != 0)
+			{
+				double route;
+				route = roundf(sqrt(tempA.x * tempA.x + tempA.y * tempA.y));
+
+				temp.x = (tempA.x / route * cos(radian) - tempA.y / route * sin(radian)) * route;
+				temp.y = (tempA.y / route * cos(radian) + tempA.x / route * sin(radian)) * route;
+
+				if (abs(temp.x + C.x) >= window.w / 2)
+				{
+					isGood = false;
+					break;
+				}
+				else if (abs(temp.y + C.y) >= window.h / 2)
+				{
+					isGood = false;
+					break;
+				}
+				
+			}
+		}
+		if (isGood)
+		{
+			for (int i = 0; i < points.size(); ++i)
+			{
+				Point temp{ 0,0 };
+				Point tempA{ 0,0 };
+
+				tempA.x = points[i].x - C.x;
+				tempA.y = points[i].y - C.y;
+
+				if (tempA.x != 0 || tempA.y != 0)
+				{
+					double route;
+					route = roundf(sqrt(tempA.x * tempA.x + tempA.y * tempA.y));
+
+					temp.x = (tempA.x / route * cos(radian) - tempA.y / route * sin(radian)) * route;
+					temp.y = (tempA.y / route * cos(radian) + tempA.x / route * sin(radian)) * route;
+
+					points[i].x = temp.x + C.x;
+					points[i].y = temp.y + C.y;
+				}
+			}
+		}
+	}
+
 	void Move(bool isGravity)
 	{
-		if (SDL_abs(A.x) >= window.w / 2 || SDL_abs(B.x) >= window.w / 2)
-			V.x *= -1;
+		for (int i = 0; i < points.size(); ++i)
+		{
+			if (abs(points[i].x + V.x * speed) >= window.w / 2)
+			{
+				V.x *= -1;
+				break;
+			}
+		}	
+		for (int i = 0; i < points.size(); ++i)
+		{
+			if (abs(points[i].y + V.y * speed) >= window.h / 2)
+			{
+				V.y *= -1;
+				break;
+			}
+		}			
 
-		if (SDL_abs(A.y) >= window.h / 2 || SDL_abs(B.y) >= window.h / 2)
-			V.y *= -1;
-
-		Normalize();
-		double gravity = 1;
+		double gravity = 0.03;
 
 		if (isGravity)
 		{
-			if (window.h / 2 <= A.y)
-				V.y *= 0;
-			else if (V.y != 0)
+			Point g{ 0, -1};
+			V.y += g.y * gravity;
+
+			for (int i = 0; i < points.size(); ++i)
 			{
-				Point g{ 0,-0.1 };
-
-				V.y += g.y * gravity;
-
-
+				if (window.h / 2 <= abs(points[i].y) + speed)
+				{
+					std::cout << "ups\n";
+					V.y = 0;
+					break;
+				}
 			}
 		}
 
-		A.x += V.x * speed;
-		B.x += V.x * speed;
+		Normalize();
 
-		A.y -= V.y * speed;
-		B.y -= V.y * speed;
+		for (int i = 0; i < points.size(); ++i)
+		{
+			points[i].x += V.x * speed;
+			points[i].y += V.y * speed;
 
-
-
-		std::cout << (int)A.x << " " << (int)A.y << " " << (int)B.x << " " << (int)B.y << "\n";
+			/* 
+			//TEST
+			int x0 = wWidth / 2;
+			int y0 = wHeight / 2;
+			SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+			SDL_RenderDrawLine(ren, x0 + points[i].x, y0 - points[i].y, x0 + points[i].x, y0 + window.h / 2);
+			*/
+		}
 	}
 	bool ponce = false;
+
 private:
 
 	SDL_Rect window{ 100, 100, wWidth - 200, wHeight - 200 };
 
 };
-
+/*
 void Collision(Square& sq1, Square& sq2)
 {
-
-	Point c1{ sq1.B.x - abs(sq1.B.x - sq1.A.x) / 2, sq1.B.y - abs(sq1.B.y - sq1.A.y) / 2 };
-	Point c2{ sq2.B.x - abs(sq2.B.x - sq2.A.x) / 2, sq2.B.y - SDL_abs(sq2.B.y - sq2.A.y) / 2 };
-
+	
+	Point c1{ sq1.B.x - abs(sq1.B.x - sq1.A.x) / 2, sq1.B.y + abs(sq1.B.y - sq1.A.y) / 2 };
+	Point c2{ sq2.B.x - abs(sq2.B.x - sq2.A.x) / 2, sq2.B.y + SDL_abs(sq2.B.y - sq2.A.y) / 2 };
+	
 	//TEST
 	//std::cout << sq1.A.x << " " << sq1.A.y << " " << sq1.B.x << " " << sq1.B.y << " ";
 	//std::cout << ":" << c1.x << ": :" << c1.y << ":\n";
@@ -195,14 +295,13 @@ void Collision(Square& sq1, Square& sq2)
 		sq1.ponce = false;
 		sq2.ponce = false;
 	}
-
 }
-
+*/
 void showAxes(int x0, int y0)
 {
 	SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-	SDL_RenderDrawLine(ren, x0, y0 - 400, x0, y0 + 400);
-	SDL_RenderDrawLine(ren, x0 + 1000, y0, x0 - 1000, y0);
+	SDL_RenderDrawLine(ren, x0, y0 - wHeight / 2, x0, y0 + wHeight / 2);
+	SDL_RenderDrawLine(ren, x0 + wWidth / 2, y0, x0 - wWidth / 2, y0);
 }
 
 double Speed(double pixels)
@@ -227,28 +326,53 @@ int main(int argc, char* arcv[])
 
 	ren = SDL_CreateRenderer(win, NULL, SDL_RENDERER_ACCELERATED);
 
-
-
-	Point v(100, 51);
-	Point A(-100, -20);
-	Point B(-60, 20);
-
-	SDL_Color s1{ 255,0,0,255 };
 	double speed(0);
 
+	// hard figure
+
+	Point v(7, 5);
+	Point A(-170, -80);
+	Point B(-130, -60);
+	Point C(-170, -10);
+	Point D(-220, -50);
+	
+	std::vector <Point> f;
+
+	f.push_back(A);
+	f.push_back(B);
+	f.push_back(C);
+	f.push_back(D);
+
+	SDL_Color s1{ 255,0,0,255 };
 
 	speed = Speed(500);
 
-	Square square1(A.x, A.y, B.x, B.y, v.x, v.y, s1, speed);
+	Figure figure(f, v.x, v.y, s1, speed);
 
-	v = { -41, 26 };
-	A = { -20, -20 };
-	B = { 20, 20 };
+	// hard figure
 
-	SDL_Color s2{ 255,0,255,255 };
+	std::vector <Point> points_circle;
+	//circle
+	{
+		std::cout << "\n";
+		int points_count = 12;
+		double radius = 40;
+		Point center(-10, 100);
+		
+		for (int i = 0; i < points_count; ++i)
+		{
+			double rad = (i * 2 * M_PI) / points_count;
+			std::cout << rad << "\n";
+			double x = center.x + (cos(rad) * radius);
+			double y = center.y + (sin(rad) * radius);
+			Point point(x, y);
+			points_circle.push_back(point);
+			std::cout << i << ": { x: " << x << " y: " << y << " }\n";
+		}
+	}
 
-	speed = Speed(400);
-	Square square2(A.x, A.y, B.x, B.y, v.x, v.y, s2, speed);
+	Figure circle(points_circle, v.x, v.y, s1, speed);
+	//circle
 
 	int x0 = wWidth / 2;
 	int y0 = wHeight / 2;
@@ -257,9 +381,13 @@ int main(int argc, char* arcv[])
 	double squareTime = SDL_GetTicks64();
 	bool isPause = true;
 	bool isProcess = true;
+
+	int a = 0.1;
 	while (isProcess)
 	{
-
+		//LOGIC PART
+		
+		//events
 		SDL_Event ev;
 		while (SDL_PollEvent(&ev))
 			switch (ev.type)
@@ -271,27 +399,43 @@ int main(int argc, char* arcv[])
 					break;
 				case SDL_SCANCODE_SPACE: isPause ? isPause = false : isPause = true;
 					break;
+				case SDL_SCANCODE_LEFT: speed - 10 > 0 ? speed -= 10 : speed = speed;
+					break;
+				case SDL_SCANCODE_RIGHT: speed +=10;
+					break;
 				}
 				break;
 			case SDL_QUIT:isProcess = false;
 			}
 
+		//Point CenterOfRotate = { 0, 0 };
+		Point CenterOfRotate = figure.points[0];
+		//If not pause that objects moved
 		if (!isPause)
 		{
-			square1.Move(1);
-			square2.Move(1);
+			
+			double dist = speed / 10;
+			//figure.Rotate(dist, x0, y0, CenterOfRotate);
+
+			figure.Move(false);
+			circle.Move(false);
 		}
 
-		Collision(square1, square2);
+		//Collision(square1, square2);
 
-		//	showAxes(x0, y0);
-
+		//GRAPHIC PART
+		showAxes(x0, y0);
 
 		timeout = SDL_GetTicks64() + delay;
 		while (SDL_GetTicks64() < timeout)
-		{
-			square1.Show(x0, y0);
-			square2.Show(x0, y0);
+		{			
+			Point CenterOfMass = figure.CenterMass();
+			figure.Show(x0, y0);
+			circle.Show(x0, y0);
+			//SDL_RenderDrawLine(ren, x0 + CenterOfRotate.x, y0 - CenterOfRotate.y, x0 + CenterOfMass.x, y0 - CenterOfMass.y);
+
+			SDL_SetRenderDrawColor(ren, 255, 0, 255, 255);
+			SDL_RenderDrawLine(ren, x0 + CenterOfMass.x + figure.V.x * 20, y0 - CenterOfMass.y - figure.V.y * 20, x0 + CenterOfMass.x, y0 - CenterOfMass.y);
 		}
 
 		SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
